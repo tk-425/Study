a7b14dda96e673ea426ab1fef7eeccc73013737f
tk-425 <terrykang425@gmail.com>
2023-01-06 17:16:13 -0500

    maven n17 multi-module update

---
+++ Apache Maven/n17-multi-module-project/web-app/src/main/java/com/mavendemo/controllers/UserController.java
@@ -0,0 +1,12 @@
+package com.mavendemo.controllers;
+
+import com.mavendemo.domain.UserCommand;
+import com.mavendemo.entities.User;
+
+public class UserController {
+
+  User saveUser(UserCommand command) {
+    // not a real implementation
+    return new User();
+  }
+}
