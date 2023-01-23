bf8a2621ce01c5a4f7b527e2e51ca2e4d230e9a5
trry <54043933+tk-425@users.noreply.github.com>
2023-01-23 04:21:55 -0500

    mock test

--- Spring Framework/In28Minutes - Spring Framework Master Class/n03-mockito/src/test/java/com/springdemo/n03mockito/business/BusinessImplMockTest.java
+++ Spring Framework/In28Minutes - Spring Framework Master Class/n03-mockito/src/test/java/com/springdemo/n03mockito/business/BusinessImplMockTest.java
@@ -1,26 +1,32 @@
 package com.springdemo.n03mockito.business;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class BusinessImplMockTest {
+
+  @Mock
+  private DataService dataServiceMock;
+
+  @InjectMocks
+  private BusinessImpl business;
 
   @Test
   void test1() {
-    DataService serviceMock = mock(DataService.class);
-    when(serviceMock.retrieveAllData()).thenReturn(new int[] {12, 5, 74});
-    BusinessImpl business = new BusinessImpl(serviceMock);
+    when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {12, 5, 74});
     assertEquals(74, business.findTheGreatestFromAllData());
   }
 
   @Test
   void test2() {
-    DataService serviceMock = mock(DataService.class);
-    when(serviceMock.retrieveAllData()).thenReturn(new int[] {0, 0, 0});
-    BusinessImpl business = new BusinessImpl(serviceMock);
+    when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {0, 0, 0});
     assertEquals(0, business.findTheGreatestFromAllData());
   }
 }
