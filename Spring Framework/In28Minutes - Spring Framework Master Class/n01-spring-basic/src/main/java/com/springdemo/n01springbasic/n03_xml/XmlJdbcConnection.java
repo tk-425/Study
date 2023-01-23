d66fcaf706bd8f86b3da55fdd9bc1c61542552a5
trry <54043933+tk-425@users.noreply.github.com>
2023-01-22 21:52:23 -0500

    using xml

---
+++ Spring Framework/In28Minutes - Spring Framework Master Class/n01-spring-basic/src/main/java/com/springdemo/n01springbasic/n03_xml/XmlJdbcConnection.java
@@ -0,0 +1,10 @@
+package com.springdemo.n01springbasic.n03_xml;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class XmlJdbcConnection {
+  public XmlJdbcConnection() {
+    System.out.println("*** XML JDBC Connection ***");
+  }
+}
