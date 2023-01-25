b2c383d55d4a510683946650281ddcc6e95889c9
trry <54043933+tk-425@users.noreply.github.com>
2023-01-25 02:38:32 -0500

    spring boot

---
+++ Spring Framework/In28Minutes - Spring Framework Master Class/n04-spring-boot/src/main/java/com/springdemo/n04springboot/N04SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.springdemo.n04springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class N04SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(N04SpringBootApplication.class, args);
+	}
+
+}
